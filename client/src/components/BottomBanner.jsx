2056b850fea542e7b9a5bf1f4dcd4d78dac167cb
ATarjun007 <atarjun2000@gmail.com>
2025-05-24 14:00:23 +0530

    product card, bestseller code add

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const BottomBanner = () => {
   return (
-    <div className=' relative mt-24'>
-        <img src={assets.bottom_banner_image} alt="banner" className='w-full hidden md:block' />
-        <img src={assets.bottom_banner_image_sm} alt="banner" className='w-full md:hidden' />
-    </div>
-  )
-}
+    <div className=" relative mt-24">
+      <img
+        src={assets.bottom_banner_image}
+        alt="banner"
+        className="w-full hidden md:block"
+      />
+      <img
+        src={assets.bottom_banner_image_sm}
+        alt="banner"
+        className="w-full md:hidden"
+      />
+      <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24 "></div>
+      <div>
+        <h1 className="text-2xl md:text-3xl font-semibold text-green">Why we are the best</h1>
+      </div>
+      </div>
+  );
+};
 
-export default BottomBanner
+export default BottomBanner;
